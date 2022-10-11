0ccc13b64a1204dc4092812010f6e95491930b4c
Calvin Seamons <calvin@pop-os.localdomain>
2022-10-11 08:09:45 -0600

    add main updates, api costs money so this project is probably dead

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ My attempt at a GeoGuessr game
 ## Kivy
 Linux/Mac Soluion
 Kivy is sort of strange, if it is not found try running `python3 -m pip install "kivy[base]" kivy_examples`
+
+Same with:
+`pip  install google_streetview` 
  
--- main.py
+++ main.py
@@ -15,6 +15,23 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 class HomeScreen(Screen):
     pass
 
+class PlayScreen(Screen):
+    import google_streetview.api
+    params = [{'size':'600x300',
+               'location':'46.414382,10.012988',
+               'heading':'151.78',
+               'pitch':'-0.76',
+               'key':'dev_test'}]
+    result=google_streetview.api.results(params)
+    def say_hi(self):
+        print("wow")
+    pass
+
+class Instructions(Screen):
+    pass
+
+class HighScore(Screen):
+    pass
 
 # This is the main GeoGuessr App/Frame, launched via gg, to contains navigation to sub frames.
 class GeoGuessr(App):
@@ -24,9 +41,13 @@ class GeoGuessr(App):
         global sm 
         sm = ScreenManager()
         Builder.load_file("widgets/homescreen.kv")
+        Builder.load_file("widgets/playscreen.kv")
+        Builder.load_file("widgets/highscore.kv")
+        Builder.load_file("widgets/instructions.kv")
         sm.add_widget(HomeScreen(name="menu"))
-        #sm.add_widget(Builder.load_file("widgets/playscreen.kv"))
-        #sm.add_wdget(Builder.load_file("widgets/highscore.kv"))
+        sm.add_widget(PlayScreen(name="playscreen"))
+        sm.add_widget(Instructions(name="instructions"))
+        sm.add_widget(HighScore(name="highscore"))
         return sm
 
 
